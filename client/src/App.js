0d3c6c2f26384a389053d4961204fb0c95173dc3
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-01-17 12:55:01 -0600

    adding heroku to restructured folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,23 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import logo from './book.png';
-import './App.css';
-import NoMatch from "./pages/NoMatch";
-import Saved from "./pages/Saved";
-import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
-import Gallery from './components/Gallery/Gallery.js';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from './components/Nav/index.js';
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: '',
-      items: []
-    };
-  }
-
-  search() {
-    const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
-    fetch(`${API_URL}${this.state.query}`)
-      .then(response => response.json())
-      .then(json => {
-        let { items } = json;
-        this.setState({ items })
-      }); // todo: Add a catch method here in case the API call fails
-  }
-
-  render() {
-
-    return (
-      <div className="App container">
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <a className="nav-link active text-dark" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="#">Saved</a>
-          </li>
-        </ul>
-        {/* <Router>
-          <div>
-          <Nav />
-          <Switch> */}
-            {/* <Route exact path="/" component={Gallery} /> */}
-            {/* <Route exact path="/saved" component={Saved} /> */}
-            {/* <Route component={NoMatch} /> */}
-          {/* </Switch>
-          </div>
-        </Router> */}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Google Books</h1>
-        </header>
-        <div className="container main-content">
-          <FormGroup>
-            <InputGroup>
-              <FormControl type="text" placeholder="Search for a book"
-                onChange={event => this.setState({ query: event.target.value })}
-                onKeyPress={event => {
-                  if ('Enter' === event.key) {
-                    this.search();
-                  }
-                }} />
-              <InputGroup.Addon onClick={() => this.search()}>
-                <Glyphicon glyph="search"></Glyphicon>
-              </InputGroup.Addon>
-            </InputGroup>
-          </FormGroup>
-          <Gallery items={this.state.items} />
-        </div>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
---
+++ controllers/googleController.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        console.log("req", req);
+        console.log("req.query: ", req.query);
+        const { query: params } = req;
+        console.log("params: ", params);
+        axios
+        .get("https://www.googleapis.com/books/v1/volumes", {
+            params
+        })
+        .then(results =>
+            results.data.items.filter(
+                result =>
+                result.volumeInfo.title &&
+                result.volumeInfo.infoLink &&
+                result.volumeInfo.authors &&
+                result.volumeInfo.description &&
+                result.volumeInfo.imageLinks &&
+                result.volumeInfo.imageLinks.thumbnail
+            )
+        )
+        .then(apiBooks =>
+            db.Book.find().then(dbBooks =>
+                apiBooks.filter(apiBook =>
+                    dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+                    )
+                )
+            )
+            .then(books => res.json(books))
+            .catch(err => res.status(422).json(err));
+    }
+};
